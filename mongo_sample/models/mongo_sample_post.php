386d5f8eff37671d79294d01c115e225c475d39e
Takayuki Miwa <i@tkyk.name>
2010-02-14 09:59:14 +0900

    Changing the connection name for mongo_sample

--- mongo_sample/models/mongo_sample_post.php
+++ mongo_sample/models/mongo_sample_post.php
@@ -1,7 +1,7 @@
 <?php
 
 class MongoSamplePost extends MongoSampleAppModel {
-	var $useDbConfig = 'mongo';
+	var $useDbConfig = 'mongo_sample';
 	var $mongoSchema = array(
 			'created'=>array('type'=>'date'),
 			'modified'=>array('type'=>'date'),
